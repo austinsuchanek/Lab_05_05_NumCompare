3725355a1a0ee80ba424ca8bb612694354e11443
austinsuchanek <suchanad@mail.uc.edu>
2022-09-23 19:43:32 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        double num1 = 4;
+        double num2 = 30;
+
+        if (num1 == num2) {
+            System.out.println("Both of the numbers you entered are equal");
+        } else if (num1 < num2)
+        {
+            System.out.println("The second number you entered is greater");
+        } else
+        {
+            System.out.println("The first number you entered is greater");
+        }
+
+    }
+}
